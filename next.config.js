0465409e457852c57bbbb9537f0dbaeccde19f35
Andreapdc <154647619+Andreapdc@users.noreply.github.com>
2025-01-21 17:15:41 +0100

    Fix font and Next.js configuration

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['www.artic.edu', 'upload.wikimedia.org', 'images.metmuseum.org'],
-  },
-  experimental: {
-    serverActions: true,
-  },
+  }
 }
 
 module.exports = nextConfig
